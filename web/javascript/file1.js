826f7df9d8c412aeef7c64d600c906558a223470
Muhammad Talha <mtalha0018@gmail.com>
2025-12-19 12:02:11 +0500

    JS intro

---
+++ web/javascript/file1.js
@@ -0,0 +1,14 @@
+// arrays = [] => indexes
+//     0        1         2      3 
+// ["Shoes", "Fruits", "Sports", 23]
+
+// object = {} => keys,value
+//   key       value
+// {name: "Alexa Samith", age: 23}
+// null :   object(empty)
+// arrays of object = [{},{}]
+users = [
+    {name: "Sam", age: 20},
+    {name: "Alice", age: 26},
+];
+console.log("From external js file")
