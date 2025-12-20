e5d84db7f7430392f45b87e3d72fff7c6c108164
Muhammad Talha <mtalha0018@gmail.com>
2025-12-20 11:03:27 +0500

    js: avg program,, value by user

--- web/javascript/file2.js
+++ web/javascript/file2.js
@@ -8,11 +8,23 @@
 // var c = a+b;
 // console.log(c);
 
-var english    = 66;
-var urdu       = 78;
-var computer   = 88;
-var sst        = 34;
-var math       = 93;
+// var english    = 66;
+// var urdu       = 78;
+// var computer   = 88;
+// var sst        = 34;
+// var math       = 93;
 
-var avg = (english + urdu + computer + sst + math) / 5;
-console.log(avg);
+// var avg = (english + urdu + computer + sst + math) / 5;
+// console.log(avg);
+
+function printavg() {
+    var english    = parseInt(document.getElementById("eng").value);
+    var urdu       = parseInt(document.getElementById("ur").value);
+    var computer   = parseInt(document.getElementById("com").value);
+    var sst        = parseInt(document.getElementById("sst").value);
+    var math       = parseInt(document.getElementById("math").value);
+    var result     = document.getElementById("result");
+
+    var avg = (english + urdu + computer + sst + math) / 5;
+    result.innerText = avg;
+}
