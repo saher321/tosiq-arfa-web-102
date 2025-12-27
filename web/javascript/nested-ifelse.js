99df399d6f7b357cbee56ed64ac19db5c4b252fc
Muhammad Talha <mtalha0018@gmail.com>
2025-12-27 10:39:06 +0500

    gaurded clause

--- web/javascript/nested-ifelse.js
+++ web/javascript/nested-ifelse.js
@@ -1,24 +1,30 @@
 
 function showResult() {
-
     var userRole        = document.getElementById("role").value;
     var userPassword    = document.getElementById("psw").value;
-
+    console.log(userRole);
     var status  = document.getElementById("status");
 
-    var role = "Admin";
+    var role = "admin";
     var password = 1234;
 
-    if ( userRole == role ) {
-        if ( userPassword == password ) {
-            status.innerText = "Loggedin successful!";
-            return;
-        } else {
-            status.innerText = "Password is incorrect"
-            return;
-        }
-    } else {
-        status.innerText = "Unauthorized";
-        return;
-    }
+    // Gaurded clause
+    if ( userRole != role ) return status.innerText = "Unauthorized";
+    if ( userPassword != password ) return status.innerText = "Password is incorrect";
+
+    status.innerText = "Loggedin successful!";
+
+    // nested if else
+    // if ( userRole == role ) {
+    //     if ( userPassword == password ) {
+    //         status.innerText = "Loggedin successful!";
+    //         return;
+    //     } else {
+    //         status.innerText = "Password is incorrect"
+    //         return;
+    //     }
+    // } else {
+    //     status.innerText = "Unauthorized";
+    //     return;
+    // }
 }
