cf5a4c193a0d4367943baa84babf3f22dd8c0565
Muhammad Talha <mtalha0018@gmail.com>
2025-12-27 10:52:09 +0500

    for loop

---
+++ web/javascript/forloop.js
@@ -0,0 +1,18 @@
+showResult();
+
+function showResult() {
+
+    // var declaration
+    // adding condition
+    // increament 
+    for (var a = 1; a <= 10; a++) {
+        console.log(`2 x ${a} = ${a * 2}`)
+        // console.log("2 x " + a + " = " + a*2)
+    }
+}
+
+// dry run
+// a = 1, a < 10 true; 1;
+// a = 2, a < 10 true; 2;
+// a = 9, a < 10 true; 9;
+// a = 10, a < 10 false;
