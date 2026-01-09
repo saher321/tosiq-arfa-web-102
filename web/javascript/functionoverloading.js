4c54a64750e9e804da578be5f2b9ea6d3431a0d0
Muhammad Talha <mtalha0018@gmail.com>
2026-01-09 11:34:15 +0500

    Function overLoading

---
+++ web/javascript/functionoverloading.js
@@ -0,0 +1,11 @@
+
+function greetings (name, timeOfDay) {
+    if (!name && !timeOfDay) return "Hello";
+    if (name && !timeOfDay) return `Hello ${name}`;
+    if (!name && timeOfDay) return `Good ${timeOfDay}`;
+    if (name && timeOfDay) return `Hello ${name}, Good ${timeOfDay}`;
+}
+console.log(greetings("", ""))
+console.log(greetings("Mike", ""))
+console.log(greetings("", "Evening"))
+console.log(greetings("Mike", "Evening"))
