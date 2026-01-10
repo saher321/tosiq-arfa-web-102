b1c0072965a203d3e9cc1608b48fc75ef14e7586
Muhammad Talha <mtalha0018@gmail.com>
2026-01-10 12:57:05 +0500

    Show hide password

--- web/jquery/page.js
+++ web/jquery/page.js
@@ -1,10 +1,14 @@
 $(document).ready(function (){
 
-    // alert("Alert in Jquery")
+    $("#cb").on("change", function (){
+        let isChecked = $("#cb").is(":checked");
+        isChecked ? $("#pswd").attr("type", "text") : $("#pswd").attr("type", "password")
+    })
+
 
     $("#btn").on("click", function () {
         let myName = $("#name").val();
         $("p").html(`<h1> ${myName} </h1>`)
-    })
+    });
 
 });
