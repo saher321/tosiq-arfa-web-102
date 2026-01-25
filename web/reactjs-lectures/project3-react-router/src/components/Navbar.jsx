21e3206832cbc693170fc0cd44c4437fafe4883f
= <=>
2026-01-25 11:52:59 +0500

    active link

--- web/reactjs-lectures/project3-react-router/src/components/Navbar.jsx
+++ web/reactjs-lectures/project3-react-router/src/components/Navbar.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { NavLink } from 'react-router'
 const Navbar = () => {
+
+  const links = [
+    { url: "/", title: "Home" },
+    { url: "/users", title: "Users" },
+    { url: "/recipes", title: "Recipes" },
+    { url: "/about-us", title: "About" },
+  ];
+
+  const active = "font-bold text-xl py-3 px-5 rounded-full bg-green-200 text-black"
+  const inActive = "font-bold text-xl py-3 px-5 rounded-full hover:bg-green-200 hover:text-black"
+
   return (
     <div className='flex items-center justify-center'>
       <nav className='flex gap-3 p-3 my-4 bg-black text-white w-fit rounded-full shadow-lg'>
-        <NavLink to="/" className='font-bold text-xl py-3 px-5 rounded-full hover:bg-green-200 hover:text-black'>Home</NavLink>
-        <NavLink to="/users" className='font-bold  text-xl py-3 px-5 rounded-full hover:bg-green-200 hover:text-black'>Users</NavLink>
-        <NavLink to="/recipes" className='font-bold  text-xl py-3 px-5 rounded-full hover:bg-green-200 hover:text-black'>Recipes</NavLink>
-        <NavLink to="/about-us" className='font-bold text-xl py-3 px-5 rounded-full hover:bg-green-200 hover:text-black'>About</NavLink>
+        {
+          links.map((link) => {
+            return (
+              <NavLink to={link.url} 
+              className={({isActive}) => isActive ? active : inActive}
+              >
+                {link.title}
+              </NavLink>
+            )
+          })
+        }
       </nav>
     </div>
   )
--- web/reactjs-lectures/project3-react-router/src/components/RecipeItem.jsx
+++ web/reactjs-lectures/project3-react-router/src/components/RecipeItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { FaStar } from "react-icons/fa";
+import { MdLocalDining } from "react-icons/md";
+import { GiCook } from "react-icons/gi";
+import { BiSolidDish } from "react-icons/bi";
 
 const RecipeItem = ({recipe}) => {
   return (
     <div className='rounded-lg shadow hover:shadow-lg bg-white'>
-      <div className='h-44 w-full relative' style={{backgroundImage: `url(${recipe.image})`, backgroundPosition: "center", backgroundSize: "cover", borderTopLeftRadius: "8px", borderTopRightRadius: "8px"}}>
+      <div className='h-44 w-full relative' style={{
+        backgroundImage: `url(${recipe.image})`, 
+        backgroundPosition: "center", 
+        backgroundSize: "cover", 
+        borderTopLeftRadius: "8px", 
+        borderTopRightRadius: "8px"
+        }}>
         <span className='absolute top-[5%] right-[5%] bg-white rounded p-1'> 
             <FaStar className='text-yellow-500 inline -mt-1'/> { recipe.rating } 
         </span>
@@ -14,6 +23,37 @@ const RecipeItem = ({recipe}) => {
         <div className='font-bold text-sm'>
             {recipe.name}
         </div>
+        <div className='my-3 mx-6 flex items-center justify-between'>
+            <div className='text-center'>
+                <MdLocalDining className='inline text-zinc-600' />
+                <div>{recipe.servings}</div>
+            </div>
+            <div className='text-center'>
+                <GiCook className='inline text-zinc-600' />
+                <div>{recipe.difficulty}</div>
+            </div>
+            <div className='text-center'>
+                <BiSolidDish className='inline text-zinc-600' />
+                <div>{recipe.cuisine}</div>
+            </div>
+        </div>
+
+        
+            {
+                recipe?.tags?.length > 0 &&
+                <>
+                <hr className='text-zinc-200'/>
+                <div className='mt-3 flex gap-1 text-xs'> 
+                    {
+                        recipe.tags.map((tag)=> {
+                            return (
+                                <div className='p-1 rounded-lg bg-purple-200 text-purple-800'>{tag}</div>
+                            )
+                        })
+                    }
+                </div>
+                </>
+            }
       </div>
     </div>
   )
