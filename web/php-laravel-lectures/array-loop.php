95757e3c50168f918624739155841404ae1caa0a
= <pnymeet@gmail.com>
2026-02-14 11:41:56 +0500

    form data with script file (student_form.php)

--- web/php-laravel-lectures/array-loop.php
+++ web/php-laravel-lectures/array-loop.php
@@ -1,21 +1,44 @@
 <?php
 
-    $index_arr1 = ["Shoes", "USB Drive", "Computer", "Key"];
+    if (isset($_POST["submit"])){
+        $name = $_POST["username"];
 
-    foreach($index_arr1 as $item) {
-        echo $item. "<br>";
+        echo $name; 
     }
+    // $index_arr1 = ["Shoes", "USB Drive", "Computer", "Key"];
 
+    // foreach($index_arr1 as $item) {
+    //     echo $item. "<br>";
+    // }
+    // // list.map((item) => {}) 
 
-    $associative_array = [
-        "name" => "Talha",
-        "age" => 45,
-        "email" => "talha@email.com",
-        "hobbies" => ["Reading books", "Browsing internet"]
-    ];
+    // $associative_array = [
+    //     "name" => "Talha",
+    //     "age" => 45,
+    //     "email" => "talha@email.com",
+    //     "hobbies" => "Reading books"
+    // ];
 
-    
-    // echo $index_arr1[2] . "<br />";
-    echo $associative_array["email"] . " ". $associative_array["hobbies"][1];
+    // // echo $index_arr1[2] . "<br />";
+    // echo $associative_array["email"] . " ". $associative_array["hobbies"];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+        <form method="post">
+            <input type="text" name="username" placeholder="Provide username">
+
+            <button type="submit" name="submit">Submit</button>
+
+        </form>
+</body>
+</html>
---
+++ web/php-laravel-lectures/student-form.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST["username"];
+$email = $_POST["useremail"];
+$password = $_POST["pswd"];
+
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $password;
+echo "<br>";
+?>
