ef65cc6b02244a002903e9dab23f38f6866f7c81
Muhammad Talha <pnymeet@gmail.com>
2026-04-04 11:35:16 +0500

    mern-intro

---
+++ web/mernstack/mern-todos/backend/index.js
@@ -0,0 +1,17 @@
+// pkgs: epxress, dotenv, nodemon, mongoose, nanoid, nodemailer, jwt
+// JASON web token: 534t3f34f43f43f.4f34rr23r3a32r.f23223
+
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT
+
+app.get('/', (req, res)=>{
+    return res.send({status: true, message: "Server is started"})
+})
+
+app.listen( PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
