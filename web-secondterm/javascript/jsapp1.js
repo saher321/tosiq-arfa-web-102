3491fcdd9d3de54500f3336592af9b4df5c6325d
PNY Training <pnymeet@gmail.com>
2026-05-16 13:24:21 +0500

    INTRO SECOND TERM

---
+++ web-secondterm/javascript/jsapp1.js
@@ -0,0 +1,15 @@
+// DATA TYPES //
+// number: 123, 23.4
+// string: 'word, W', "Word, w",
+// object: product = { name: "Toy car", price: 3999  }
+// product.price
+
+// indexes            0         1        2       3        4
+// array: clrs = [ "Purple", "White", "Black", "Red", "Orange" ]
+// clrs.length = 5
+
+// clrs[3]
+// reguler : rgx = /?a-Z/(0-5)?*/
+// Boolean: true / false, 0 / 1
+// null
+// function getUsers()
---
+++ web-secondterm/readme.txt
@@ -0,0 +1,14 @@
+2nd term
+
+JS
+ReactJs
+
+Python core
+Python with DRF (DJANGO REST FRAMEWORK) :: apis
+
+c++
+c#, asp.net
+javascriptboot
+
+github url 
+https://github.com/saher321/tosiq-arfa-web-102
