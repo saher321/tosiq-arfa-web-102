9f23bbc645141551f229d56ca1dcb79d34b197b0
Tosiq <pnymeet@gmail.com>
2026-05-17 13:43:38 +0500

    let const var

---
+++ web-secondterm/javascript/let-const-var.js
@@ -0,0 +1,13 @@
+var age = 32
+var age = 20
+console.log(age)
+// re-declare and re-initialize
+
+let name = "John"
+name = "Alex"
+console.log(name)
+// not declare but re-initialize
+
+const email = "john@gmail.com"
+console.log(email)
+// not declaration or re-initialize
