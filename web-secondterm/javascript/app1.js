0e2fe6bdc9d8976fd5fdf2eabe520119c840d9d7
PNY Training <pnymeet@gmail.com>
2026-05-17 12:39:27 +0500

    c to f

--- web-secondterm/javascript/app1.js
+++ web-secondterm/javascript/app1.js
@@ -1,7 +1,15 @@
 
 function applyColor() {
     let color = document.getElementById("clr").value
-    console.log(color)
 
     document.body.style.backgroundColor = color
+}
+
+function convertToC() {
+    let c = document.getElementById("cel").value
+    let result = document.getElementById("result")
+
+    let f = (c * 9/5) + 32
+
+    result.innerText = f;
 }
