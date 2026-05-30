70b2fc2106708fb4b96e2e3f33ec694c685dfe0b
pny <pnymeet@gmail.com>
2026-05-30 13:11:35 +0500

    icons

--- web-secondterm/nextapps/nextapp1/app/(pages)/help/page.jsx
+++ web-secondterm/nextapps/nextapp1/app/(pages)/help/page.jsx
@@ -1,13 +1,14 @@
 import Navbar from '@/app/components/Navbar'
 import React from 'react'
+import { BiAtom, BiCheckShield } from "react-icons/bi";
 
 const page = () => {
   return (
     <>
     
     <Navbar />
-    <div className='m-2 text-emerald-900 bg-emerald-200 shadow rounded p-2'>
-      Help page
+    <div className='flex gap-3 items-center m-2 text-emerald-900 bg-emerald-200 shadow rounded p-2'>
+      <BiCheckShield /> <BiAtom /> Help page
     </div>
     </>
   )
