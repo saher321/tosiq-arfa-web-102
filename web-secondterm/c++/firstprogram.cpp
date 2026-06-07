2044c55c422c83f14615b128850c2bf478735ee3
pny <pnymeet@gmail.com>
2026-06-07 12:44:56 +0500

    c++ ftoc

---
+++ web-secondterm/c++/firstprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	float fahrenheit, celsius;
+	fahrenheit = 273.4;
+	
+	celsius = (fahrenheit - 32) * 5/9;
+	
+//	cout << fahrenheit << " Fahrenheit value" << "\n";
+//	cout << "Hello, world" << endl;
+
+	cout << "Temperature from F to C is: " << celsius;	
+	
+}
