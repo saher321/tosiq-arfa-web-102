ebd68e2e457e2b93e911bcd16943396d4567221b
pny <pnymeet@gmail.com>
2026-06-07 13:40:04 +0500

    get value by user

--- web-secondterm/c++/findavg.cpp
+++ web-secondterm/c++/findavg.cpp
@@ -6,11 +6,26 @@ int main(){
 	int total = 500, sum = 0, english, urdu, math, sst, chemistry;
 	float avg, prcnt;
 	
-	english 	= 48;
-	urdu 		= 66;
-	math 		= 69;
-	sst 		= 94;
-	chemistry 	= 95;	
+//	english 	= 48;
+//	urdu 		= 66;
+//	math 		= 69;
+//	sst 		= 94;
+//	chemistry 	= 95;
+	cout << "English marks: ";
+	cin >> english;
+	
+	cout << "Urdu marks: ";
+	cin >> urdu;
+	
+	cout << "Math marks: ";
+	cin >> math;
+	
+	cout << "SST marks: ";
+	cin >> sst;	
+	
+	cout << "Chemistry marks: ";
+	cin >> chemistry;
+	cout << endl << "**** RESULT ****" << endl;
 	
 	sum 	= english + urdu + math + sst + chemistry;
 	prcnt 	= (sum * 100 ) / total;
