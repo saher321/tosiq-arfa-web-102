b458cbe5446aa300ba1da1dedc93dced5bf3226d
pny <pnymeet@gmail.com>
2026-06-13 12:33:48 +0500

    array

---
+++ web-secondterm/c++/array1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int arr[5] = {40, 32, 67, 88, -23};
+	
+//	cout << "Value: " << arr[2];
+	
+	for (int i = 0; i < 5; i++ ) {
+		cout << arr[i] << endl;
+	}
+}
