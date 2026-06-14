8a0c22e7e5f37cf758eb83538f3039659f748596
pny <pnymeet@gmail.com>
2026-06-14 13:02:52 +0500

    list-set-tuple-dictionary

--- web-secondterm/python/core/intro.py
+++ web-secondterm/python/core/intro.py
@@ -12,26 +12,31 @@ dictionary = {
 }
 
 print(dictionary['country'])
-# list of dictionaries => [
-#    { "id" : 1, "color" : "purple" },
-#    {},
-#    {},
-#    {},
-# ]
-
+list_of_dictionaries = [
+   { "id" : 1, "color" : "purple" },
+   { "id" : 2, "color" : "white" },
+   { "id" : 3, "color" : "black" },
+   { "id" : 4, "color" : "orange" },
+]
+print("List of dictionary: ", list_of_dictionaries)
 # tuple => (1,3,4,6,5,6) => immutable :: non changeable
+tupledata   = (1,3,4,6,5,6)
+print("Tuple: ", tupledata)
+
+setdata     = { 1,3,4,6,5,6 }
+print("Set: ", setdata)
 # set => { 1,3,4,6,5,6 } => mutable :: changeable
 name    = "Ali"
 age     = 34
 status  = True
 x       = 3.4
 
-print("Welcome to the Python", name, age)
-print('\n', type(name))
-print('\n', type(age))
-print('\n', type(status))
-print('\n', type(x))
+# print("Welcome to the Python", name, age)
+# print('\n', type(name))
+# print('\n', type(age))
+# print('\n', type(status))
+# print('\n', type(x))
 
-cel = float(input("Enter celsius value: "))
-fahr = (cel * 9/5) + 32
-print("Fahrenheit value is: ", fahr)
+# cel = float(input("Enter celsius value: "))
+# fahr = (cel * 9/5) + 32
+# print("Fahrenheit value is: ", fahr)
