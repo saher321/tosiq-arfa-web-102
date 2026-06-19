644489bd3b7ce2208ec47ecb178c416e05903be4
tosiq <pnymeet@gmail.com>
2026-06-19 15:35:48 +0500

    moduler file python

---
+++ web-secondterm/python/core/tempConversion.py
@@ -0,0 +1,7 @@
+import tempConversionLib
+
+cel = float(input("Enter celsius value: "))
+tempConversionLib.cToF(cel)
+
+fahr = float(input("Enter fahrenhiet value: "))
+tempConversionLib.fToC(fahr)
---
+++ web-secondterm/python/core/tempConversionLib.py
@@ -0,0 +1,7 @@
+def cToF(cel):
+    fahr = (cel * 9/5) + 32
+    print("Fahrenheit value is: ", fahr)
+
+def fToC(fahr):
+    cel = (fahr - 32) * 5/9
+    print("Celsius value is: ", cel)
