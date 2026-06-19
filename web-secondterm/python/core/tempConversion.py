c83ae577d9466fe89a2a933462cd96ad0ef043c9
tosiq <pnymeet@gmail.com>
2026-06-19 16:05:09 +0500

    task

--- web-secondterm/python/core/tempConversion.py
+++ web-secondterm/python/core/tempConversion.py
@@ -5,3 +5,14 @@ tempConversionLib.cToF(cel)
 
 fahr = float(input("Enter fahrenhiet value: "))
 tempConversionLib.fToC(fahr)
+
+# task
+# first number = 5
+# second number = 2
+# type any of ( +, -, /, * )?  -
+
+# if operator = "+":
+# sum(a, b)
+# multiplication(a, b)
+# division(a, b)
+# substraction(a, b)
