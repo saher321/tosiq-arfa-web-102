2343a836573abbdd78d6837ee0a788f387eb43bc
Tosiq <pnymeet@gmail.com>
2026-06-21 14:52:55 +0500

    list

---
+++ web-secondterm/python/core/list.py
@@ -0,0 +1,26 @@
+my_list = [23, 56, 34, 21, 45, 67, 12]
+# # my_list.append(90)
+# # my_list.insert(0, 90)
+# my_list.remove(56)
+itemCount = len(my_list)
+# my_list.sort()
+# # my_list.pop()
+# my_list.reverse()
+print(itemCount)
+
+# # for item in my_list:
+# #   print(item)
+
+# number = int(input("Enter a number: "))
+# my_list = []
+# my_list.append(number)
+# print(my_list)
+
+# TASK
+# input = any color
+# empty list
+# loop (5)
+# append color to list
+# print list
+# output
+# Purple, Blue, White, Red, Green
