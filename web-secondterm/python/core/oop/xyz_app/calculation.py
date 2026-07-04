6f83e013a0a9749345a320fcb13859a8a2ecfebc
Tosiq <pnymeet@gmail.com>
2026-07-04 15:37:44 +0500

    class import

---
+++ web-secondterm/python/core/oop/xyz_app/calculation.py
@@ -0,0 +1,8 @@
+from classes.arithmatics import Arithmatics
+
+arithmatic = Arithmatics()
+
+print("Sum of two numbers is: ", arithmatic.add(3,5))
+print("subtract of two numbers is: ", arithmatic.subtract(3,5))
+print("divide of two numbers is: ", arithmatic.divide(3,5))
+print("multiply of two numbers is: ", arithmatic.multiply(3,5))
