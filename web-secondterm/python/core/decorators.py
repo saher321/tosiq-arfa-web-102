520f85f54bb7d34c5b1a72890d4f8321b7ad7d57
Tosiq <pnymeet@gmail.com>
2026-07-05 16:02:45 +0500

    decorators

---
+++ web-secondterm/python/core/decorators.py
@@ -0,0 +1,17 @@
+# def my_dec(func):
+#     def innerFunction():
+#         print("Welcome back!")
+#         func()
+#         print("Thanks for working")
+#     return innerFunction
+
+# @my_dec
+# def greetings(msg):
+#     print(msg)
+
+# greetings("Good evening...")
+
+# output
+# Welcome back!
+# Good evening...
+# Thanks for working
