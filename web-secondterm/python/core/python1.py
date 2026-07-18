64939f36a51346e68d219c8f9b6e4a190f69409e
tosiq <pnymeet@gmail.com>
2026-07-18 15:53:25 +0500

    match, loop, while

--- web-secondterm/python/core/python1.py
+++ web-secondterm/python/core/python1.py
@@ -50,12 +50,58 @@ cel = ( fah - 32 ) * 5/9
 
 # ****** IF ELIF ********
 
-password = input("Enter any password: ")
+# password = input("Enter any password: ")
 
-if not password:
-    print("Please provide password!")
-else:
-    if len(password) > 4:
-        print(f"You entered this pass: {password}")
-    else:
-        print("Password is too short")
+# if not password:
+#     print("Please provide password!")
+# else:
+#     if len(password) > 4:
+#         print(f"You entered this pass: {password}")
+#     else:
+#         print("Password is too short")
+
+# ***** MATCH ******
+
+# print("1- Add data")
+# print("2- Delete data")
+# print("3- Update data")
+# print("4- Read data")
+
+# choice_no = int(input("Enter number 1-4: "))
+# choice_no = 0
+# match choice_no:
+#     case 1:
+#         print("Data has been added successfully!")
+#     case 2:
+#         print("Data has been deleted successfully!")
+#     case 3:
+#         print("Data has been updated successfully!")
+#     case 4:
+#         print("Records found successful!")
+#     case _:
+#         print("Invalid entry")
+
+# ******** LOOPS *******
+
+# tbl_no = int(input("Enter number to print its table: "))
+# limit = int(input("Table limit: "))
+# for i in range(1, limit+1):
+    # print(f"{tbl_no} x {i} = {tbl_no*i}")
+    # pass
+# li = [ 23, 46, 8, 956, 22, 10 ]
+# sum = 0
+# for item in li:
+#     if item % 2 == 0:
+#         sum = sum+item
+# print(f"Sum is: {sum}")
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+password = ""
+while password != "pass123":
+    password = input("Enter your password: ")
+
+print("Access granted")
