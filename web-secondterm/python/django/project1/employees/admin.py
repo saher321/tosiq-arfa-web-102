19a0fad78cd8a21b3053435a2a118fd5ead8f67f
tosiq <pnymeet@gmail.com>
2026-08-01 15:04:45 +0500

    django-admin

--- web-secondterm/python/django/project1/employees/admin.py
+++ web-secondterm/python/django/project1/employees/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Employee
+
 
 # Register your models here.
+class EmployeeData(admin.ModelAdmin):
+    list_display = ("emp_id","full_name", "email", "contact")
+
+
+admin.site.register(Employee, EmployeeData)
--- web-secondterm/python/django/project1/employees/models.py
+++ web-secondterm/python/django/project1/employees/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Employee(models.Model):
     emp_id      = models.IntegerField(blank=False)
-    full_name   = models.CharField(max_length=50,blank=False)
+    full_name   = models.CharField(max_length=50, blank=False)
     email       = models.CharField(max_length=100, blank=False)
-    contact     = models.IntegerField(blank=True)
+    contact     = models.IntegerField()
     
     # gender      = models.CharField(max_length=6 ,blank=True)
     # male, female (255 char)
+    def __str__(self):
+        return self.full_name
