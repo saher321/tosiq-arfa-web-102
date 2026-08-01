c49eeb07d03383c9b09c0470f9c8726174dfcdd6
tosiq <pnymeet@gmail.com>
2026-08-01 15:32:36 +0500

    templates

--- web-secondterm/python/django/project1/employees/views.py
+++ web-secondterm/python/django/project1/employees/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+def EmployeePage(req):
+    employee_page = loader.get_template("employees.html")
+    return HttpResponse(employee_page.render())
