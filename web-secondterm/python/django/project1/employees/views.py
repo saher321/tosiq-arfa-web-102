f2d329fdc7ac31f2695d0591decf683eee3d11cb
tosiq <pnymeet@gmail.com>
2026-08-02 14:54:20 +0500

    templates

--- web-secondterm/python/django/project1/employees/views.py
+++ web-secondterm/python/django/project1/employees/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Employee
 # Create your views here.
 
 def EmployeePage(req):
+    employees = Employee.objects.values()
+    # page = "employees.html"
+    # return render(page, req)
     employee_page = loader.get_template("employees.html")
-    return HttpResponse(employee_page.render())
+
+    context = {
+        'employees' : employees
+    }
+
+    return HttpResponse(employee_page.render(context))
