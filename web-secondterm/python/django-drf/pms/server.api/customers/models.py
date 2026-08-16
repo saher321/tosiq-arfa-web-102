7d2b7c83340756f8c83bb4cd3ee88f75ba5536ab
PNY Training <pnymeet@gmail.com>
2026-08-16 03:46:14 -0700

    .

--- web-secondterm/python/django-drf/pms/server.api/customers/models.py
+++ web-secondterm/python/django-drf/pms/server.api/customers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
